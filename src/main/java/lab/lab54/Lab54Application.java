78c9e5fcadc3399267ccd3a2084d2f11df758956
Aishashirin <aishashirina@gmail.com>
2021-02-01 15:03:49 +0600

    start

---
+++ src/main/java/lab/lab54/Lab54Application.java
@@ -0,0 +1,13 @@
+package lab.lab54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab54Application.class, args);
+    }
+
+}
---
+++ src/test/java/lab/lab54/Lab54ApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.lab54;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab54ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
